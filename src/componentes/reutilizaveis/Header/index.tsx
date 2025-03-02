60706227ee00667f82c67a6dc885debf4547fc97
Luiz Guilherme Rótulo <luizgrbt@gmail.com>
2025-03-02 16:39:46 -0300

    att

--- src/componentes/reutilizaveis/Header/index.tsx
+++ src/componentes/reutilizaveis/Header/index.tsx
@@ -11,17 +11,26 @@ const HeaderPadrao = styled.header`
     padding: 1.7rem 5rem;
 `
 
+const LinkLogoPetHop = styled(Link)`
+    text-decoration: none;
+
+    &:hover {
+        background-color:rgba(255, 255, 255, 0.21);
+        border-radius: 0.5rem;
+    }
+`
+
 function Header() {
     return (
         <HeaderPadrao>
-            <Link to='/'>
+            <LinkLogoPetHop to='/'>
                 <ReutilizavelTituloH2
                 $larguraFonte='800'
                 $tamanhoFonte='24px'
                 $corFonte='#8ED9ED'
             >
                 PETHOP.</ReutilizavelTituloH2>
-            </Link>
+            </LinkLogoPetHop>
             <NavegacaoHeader />
         </HeaderPadrao>
     );
